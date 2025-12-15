3ce3340f86a8e4661fd166343c7a68fe3c062cfb
Cee Jay <chijiokeanazodo2020@gmail.com>
2025-12-15 21:16:46 +0100

    fix: ensure base path is set for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // important for GitHub Pages with custom domain
   plugins: [react(), tailwindcss()],
 });
